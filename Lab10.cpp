c372252d9930082cc4d27831807fd28104789b53
misiagi <misiagi@o2.pl>
2014-01-14 20:42:52 +0100

    Create Lab10.cpp

---
+++ Lab10.cpp
@@ -0,0 +1,39 @@
+#include "stdafx.h"
+#include "user.h"
+#include "process.h"
+#include "history.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	user new_user;
+
+	int ids[8] = {1,2,3,4,5,6,7,8};
+	int moves[8] = {2,3,1,2,1,2,5,3};
+	int moves_time[8] = {3,2,3,5,2,5,2,5}; // maksymalne czasy, rzeczywiste ustalane sa losowo podczas symulacji procesów
+
+	new_user.init_process(8, ids, moves, moves_time);
+
+	new_user.FIFO_strategy(); //wybor strategii FIFO
+
+	new_user.sim_process();
+
+	(new_user.phistory)->display();
+
+	cout<<endl<<endl;
+
+	new_user.clear_process_vector(); //czyszczenie wektora z procesami (ich wlasciwosci ulegly zmianie w trakcie symulacji)
+
+	new_user.init_process(8, ids, moves, moves_time); //inicjalizacja procesow na nowo
+
+	new_user.SJF_strategy(); //wybor strategii SJF
+
+	new_user.sim_process();
+	
+	(new_user.phistory)->display();
+
+	getchar();
+}
+
